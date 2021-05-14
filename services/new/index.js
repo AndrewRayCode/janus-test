734467fe9617a6d19b3fef5f13f719933667edd7
Andrew Ray <andrew.ray@grandrounds.com>
2021-05-14 15:53:36 -0700

    work

--- services/new/index.js
+++ services/new/index.js
@@ -2,18 +2,115 @@ const { ApolloServer, gql } = require("apollo-server");
 const { buildFederatedSchema } = require("@apollo/federation");
 
 const typeDefs = gql`
-  type MyThing {
-    id: ID
+  extend type MemberType @key(fields: "id") {
+    id: ID! @external
+    clinicalVisits: [ClinicalVisit]
+    interactions: [Interaction]
   }
+
+  interface Interaction {
+    id: ID!
+    member: MemberType
+  }
+
+  # extend type CaseType @key(fields: "id") @provides(fields: "serviceType") {
+  # Directive "@provides" may not be used on OBJECT.
+  # extend type CaseType @key(fields: "id") {
+  #   id: ID! @external
+    # serviceType: String! @provides("serviceType")
+    # Syntax Error
+  # }
+
+  type ClinicalVisit implements Interaction {
+  # type ClinicalVisit {
+    id: ID!
+    reasonForVisit: String!
+    member: MemberType
+  }
+
   extend type Query {
-    aside: MyThing
+    # interactions: [Interaction]
+    clinicalVisits: [ClinicalVisit]
+  }
+
+  extend type Mutation {
+    createClinicalVisit(memberId: ID!): ClinicalVisit
   }
 `;
 
 const resolvers = {
-  Query: {
-    aside() {
-      return { id: 'Aside ID' };
+  /**
+   * me.clinicalVisits moving into CareAPI
+   */
+  MemberType: {
+    interactions() {
+      return [
+        {
+          __typename: 'ClinicalVisit',
+          id: 'ID From CareAPI clinicalVisits resolver',
+          reasonForVisit: 'reasonForVisit From CareAPI clinicalVisits resolver',
+        },
+        {
+          __typename: 'CaseType',
+          id: 'ID From CareAPI interactions resolver',
+          serviceType: 'serviceType From CareAPI interactions resolver',
+        }
+      ]
+    },
+
+    clinicalVisits(memberFromJarvis) {
+      console.log('got member: ', memberFromJarvis);
+
+      // const clinicalVisitsFromJarvis = query('jarvis', `
+      //   query me.clinicalVisistsBridge {
+      //     id
+      //     patient {
+      //       id
+      //     }
+      //   }
+      // `);
+
+      // return clinicalVisitsFromJarvis.map(obj => ({
+      //   ...obj,
+      //   patient: { id: obj.patient.id }
+      // }))
+
+      const clinicalVisitsFromJarvis = [{
+        __typename: 'ClinicalVisit',
+        id: 'ID From CareAPI clinicalVisits resolver',
+        reasonForVisit: 'reasonForVisit From CareAPI clinicalVisits resolver',
+        member: {
+          // Note that this memberID isn't the me { } field that we're nested
+          // inside of, it's the member of the case.
+          // id: clinicalVisitsFromJarvis[0].memberId
+          id: memberFromJarvis.id
+        }
+      }];
+
+      return clinicalVisitsFromJarvis;
+    }
+  },
+
+  /**
+   * Mutation createClinicalVisit moving into CareAPI
+   */
+  Mutation: {
+    createClinicalVisit(_, args) {
+      return {
+        __typename: 'ClinicalVisit',
+        id: 'New ClinicalVisit ID From CareAPI',
+        reasonForVisit: 'CareAPI reasonForVisit',
+        member: {
+          id: args.memberId
+        }
+      };
+    }
+  },
+  // When the mutation is called and the member is requested, this resolver
+  // powers the ClinicalVisit.member field
+  ClinicalVisit: {
+    patient(newClinicalVisit) {
+      return { __typename: 'MemberType', id: newClinicalVisit.member.id };
     }
   },
 };
--- services/old/index.js
+++ services/old/index.js
@@ -2,27 +2,119 @@ const { ApolloServer, gql } = require("apollo-server");
 const { buildFederatedSchema } = require("@apollo/federation");
 
 const typeDefs = gql`
-  type Member @key(fields: "id") {
+  interface Interaction {
     id: ID!
-    name: String
+    member: MemberType
   }
 
+  type ClinicalVisit implements Interaction {
+    id: ID!
+    reasonForVisit: String!
+    member: MemberType
+  }
+
+  type CaseType implements Interaction @key(fields: "id") {
+    id: ID!
+    serviceType: String!
+    member: MemberType
+  }
+
+  type MemberType @key(fields: "id") {
+    id: ID!
+    name: String!
+    # interactions: [Interaction]
+    # clinicalVisits: [ClinicalVisit]
+  }
+
+  # extend type Mutation {
+  #   createClinicalVisit(memberId: ID!): ClinicalVisit
+  # }
+
+  # extend type ClinicalVisit @key(fields: "id") {
+  #   id: ID! @external
+  #   member: MemberType
+  # }
+
   extend type Query {
-    me: Member
+    me: MemberType
   }
 `;
 
 const resolvers = {
+  /**
+   * me.clinicalVisits moving into CareAPI
+   */
   Query: {
     me() {
-      return users[0];
+      return {
+        id: 'Jarvis me member ID',
+        name: 'me name'
+      }
     }
   },
-  Member: {
+  MemberType: {
+    interactions() {
+      return [
+        {
+          __typename: 'ClinicalVisit',
+          id: 'ID From Jarvis clinicalVisits resolver',
+          reasonForVisit: 'reasonForVisit From Jarvis clinicalVisits resolver',
+        },
+        {
+          __typename: 'CaseType',
+          id: 'ID From Jarvis interactions resolver',
+          serviceType: 'serviceType From Jarvis interactions resolver',
+        }
+      ]
+    },
+
+    // Phase 0: Jarvis handles clincalVisits
+    clinicalVisits() {
+      return [{
+        __typename: 'ClinicalVisit',
+        id: 'ID From Jarvis clinicalVisits resolver',
+        reasonForVisit: 'reasonForVisit From Jarvis clinicalVisits resolver',
+        // member: { // WRONG!
+        //   id: '1'
+        // }
+      }];
+    },
+
+    // Phase 1: Jarvis resovles reference from careAPI
     __resolveReference(object) {
-      return users.find(user => user.id === object.id);
+      console.log('JARVIS: member reference resolver', object);
+      return {
+        id: 'ID From Jarvis reference resolver',
+        name: 'Name from Jarvis reference resolver',
+      };
+    },
+  },
+
+  /**
+   * Mutation createClinicalVisit moving into CareAPI
+   */
+  Mutation: {
+    createClinicalVisit() {
+      return {
+        __typename: 'ClinicalVisit',
+        id: 'New ID From Jarvis',
+        reasonForVisit: 'Jarvis reasonForVisit'
+      };
     }
-  }
+  },
+  /**
+   * (Phase 0) When Jarvis owns Clinical Visits, this field
+   * is called
+   */
+  ClinicalVisit: {
+    member(data) {
+      console.log('JARVIS: resolve member', data);
+      return {
+        name: 'ID from Jarvis member() resolver',
+        id: 'ID from Jarvis member() resolver'
+      };
+    }
+  },
 };
 
 const server = new ApolloServer({
@@ -46,3 +138,14 @@ const users = [
     username: "@ada"
   }
 ];
+
+/**
+ * For a top level clinical visits field, the setup works where we
+ * can define the clinical visit type across both boundaries and mark
+ * member as an entity and not load it in
+ *
+ * For the mutation case, it seems to work the same way, so I think
+ * we're ok there.
+ *
+ * For the clinicalVisit field on member
+ */
